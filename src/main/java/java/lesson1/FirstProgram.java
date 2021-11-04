e14f3af8f7dd2b87bb48a32ef779105d9238a886
Damir Iskakov <dziskakov@sberbank.ru>
2021-11-04 22:33:50 +0300

    add second hw

--- src/main/java/java/lesson1/FirstProgram.java
+++ src/main/java/java/lesson1/FirstProgram.java
@@ -1,4 +1,7 @@
+package java.lesson1;
+
 public class FirstProgram {
+
     public static void main(String[] args) {
         System.out.println("Hello, world!");
     }
---
+++ src/main/java/lesson2/SimpleMath.java
@@ -0,0 +1,17 @@
+package lesson2;
+
+public class SimpleMath {
+
+    public static void main(String[] args) {
+        System.out.println(calculateSquare(-3));
+    }
+
+    /**
+     * Возвращает квадрат числа, а где описание
+     * @param number аргумент
+     * @return результат возведения числа в квадрат
+     */
+    private static long calculateSquare(long number) {
+        return number * number;
+    }
+}
