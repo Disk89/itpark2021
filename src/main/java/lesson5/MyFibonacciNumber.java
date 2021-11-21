f889c39a2e83a442362e7111cbcc7b74ea0347a8
Damir Iskakov <dziskakov@sberbank.ru>
2021-11-21 17:33:12 +0300

    change type from int to long

--- src/main/java/lesson5/MyFibonacciNumber.java
+++ src/main/java/lesson5/MyFibonacciNumber.java
@@ -5,6 +5,7 @@ import java.util.Scanner;
 // Условие: Вывести n-ый член последовательности Фибоначчи, где параметр n запрашивается в консоли у пользователя
 public class MyFibonacciNumber {
     public static void main(String[] args) {
+        System.out.println(Long.MAX_VALUE);
         Scanner scanner = new Scanner(System.in);
         int n;
         do {
@@ -16,17 +17,17 @@ public class MyFibonacciNumber {
 
     private static void myFibNumber(int num) {
         if (num < 0) {
-            System.out.println("Вы ввели неверное число");
+            System.out.println("Вы ввели отрицательное число");
         } else {
-            int f0 = 0;
-            int f1 = 1;
-            int fn = num;
+            long f0 = 0;
+            long f1 = 1;
+            long fn = num;
             for (int i = 2; i <= num; i++) {
                 fn = f0 + f1;
                 f0 = f1;
                 f1 = fn;
             }
-            System.out.println(fn);
+            System.out.println((long) fn);
         }
     }
 }
